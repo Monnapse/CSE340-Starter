171eee5cddc8c148af0b7be2f94395ac577e5a3f
Monnapse <masonshuber@gmail.com>
2025-03-17 10:33:26 -0600

    Added routes with dynamic items using DB

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
+
 /* ******************************************
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
@@ -5,38 +8,39 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
 
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout")
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout");
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
 
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome);
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
